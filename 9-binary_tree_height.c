a2ae457e00809c178716b01f4d37af7983e5158b
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-13 15:58:42 -0500

    function calculated the height of an binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+/**
+ * returngreater - find the geater of two unsigned integer
+ *
+ * @a: first integer
+ * @b: second integer
+ * Return: the greater between the two
+ */
+size_t returngreater(size_t a, size_t b)
+{
+
+	if (a > b)
+	{
+		return (a);
+	}
+	return (b);
+
+}
+/**
+ * binary_tree_height - find out the height of the binary tree
+ *
+ * @tree: Pointer to the node root of the binary tree
+ * Return: height of the binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h, lef, rig;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (binary_tree_is_leaf(tree) == 1)
+	{
+		return (0);
+	}
+	lef = binary_tree_height(tree->left);
+	rig = binary_tree_height(tree->right);
+	h = returngreater(lef, rig) + 1;
+	return (h);
+}
