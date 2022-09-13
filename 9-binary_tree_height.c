5675074976925e23568abb95061132a83324f92c
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-13 16:09:38 -0500

    function calculated the height of an binary tree fit compiled error 3

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -31,7 +31,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (binary_tree_is_leaf(tree) == 1)
+	if ((tree->left == NULL) && (tree->right == NULL))
 	{
 		return (0);
 	}
