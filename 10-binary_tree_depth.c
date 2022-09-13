a6adecc24b2757153382ec87d402237d38171861
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-13 16:31:04 -0500

    function that calculated the depth of a node in a binary tree fit betty-doc

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,4 +1,10 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_depth - find out the depth of a node in a binary tree
+ *
+ * @tree: Pointer to the node of the binary tree
+ * Return: depth of the node
+ */
 size_t binary_tree_depth(binary_tree_t *tree)
 {
 	size_t dth;
