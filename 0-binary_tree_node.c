b687b8709499e624d85ff18bde6d2598639e7e32
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-13 05:54:14 -0500

    functio add new node to binary tree

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create a new node
+ * @parent: pointer to the node parent
+ * @value: the new value to be insert in the node
+ * Return: a new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = value;
+	new_node->parent = parent;
+	return (new_node);
+}
