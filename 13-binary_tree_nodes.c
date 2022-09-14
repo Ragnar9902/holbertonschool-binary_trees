b588540aa01e595ed08b23be0a07955d5c8ee989
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-14 14:32:26 -0500

    add function that calculated the number of nodes with one child at leaves fit compile error

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,3 +12,50 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	nodes = binary_tree_size(tree) - binary_tree_leaves(tree);
 	return (nodes);
 }
+/**
+ * binary_tree_leaves - find out the total number of leaves of the tree
+ *
+ * @tree: Pointer to the root of the binary tree
+ * Return: leaves of the binary tree
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leav, lleav, rleav;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if ((tree->left == NULL) && (tree->right == NULL))
+	{
+		return (1);
+	}
+	lleav = binary_tree_leaves(tree->left);
+	rleav = binary_tree_leaves(tree->right);
+	leav = lleav + rleav;
+	return (leav);
+}
+/**
+ * binary_tree_size - find out the total number of node of the tree
+ *
+ * @tree: Pointer to the root of the binary tree
+ * Return: size of the binary tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t sleft, sright, size;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if ((tree->left == NULL) && (tree->right == NULL))
+	{
+		return (1);
+	}
+
+	sleft = binary_tree_size(tree->left);
+	sright = binary_tree_size(tree->right);
+	size = sleft + sright + 1;
+	return (size);
+}
